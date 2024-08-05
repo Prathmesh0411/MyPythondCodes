5a2de0dbaead6bb1259c6a48370907b9ddd5277f
Prathmesh0411 <prathmeshrathod01@gmail.com>
2024-08-05 10:11:15 +0530

    Update prathmesh.py

--- prathmesh.py
+++ prathmesh.py
@@ -3,22 +3,22 @@ def removeDuplicate(d):
     for i in d:
         if i not in unique_lst:
             unique_lst.append(i)
-   return unique_lst
+    return unique_lst
 
 def union(d1,d2):
     union_lst=[]
     for i in d1:
         if i not in union_lst:
             union_lst.append(i)
-   for j in d2:
+    for j in d2:
        if j not in union_lst:
            union_lst.append(j)
-   return union_lst
+    return union_lst
 
 def diff(d1,d2):
     deff=[]
     for i in d1:
-        if not in d2:
+        if i not in d2:
            diff.append(i)
     return diff
 
