893f64f29c58e4fcbff53b31b5440aba27d48c1e
Prathmesh0411 <prathmeshrathod01@gmail.com>
2024-12-10 12:11:34 +0530

    prathmesh10.cpp

--- prathmesh10.cpp
+++ prathmesh10.cpp
@@ -1,110 +1,118 @@
 #include <iostream>
 using namespace std;
-class stack
-{
-char s[25];
-int top;
-public :
-void push(char val);
-char pop();
-bool isempty();
-bool isfull();
-void display();
-stack()
-{
-top=-1;
-}
-};
-bool stack::isempty()
-{
-if(top==-1)
-{
-}
-else
-}
-return 1;
-return 0;
-void stack::push(char val)
-{
-if(top<=24)
-{
-}
-else
-top++;
-s[top]=val;
-cout<<"\n stack is full";
-}
-char stack::pop()
-{
-char val;
-if(!isempty())
-{
-val=s[top];
-top--;
-return val;
-}
-else
-{ cout<<"\n enter stack";
-return '*';
-}
-}
-class paranthesis
-{
-char expn[25];
-stack obj;
+
+class stack {
+    char s[25];
+    int top;
+
 public:
-void read();
-void checkexpn();
+    stack() {
+        top = -1;  // Initializing stack
+    }
+
+    void push(char val);
+    char pop();
+    bool isempty();
+    bool isfull();
+    void display();
 };
-void paranthesis::read()
-{
-cout<<"\n Enter the expression : ";
-cin>>expn;
-cout<<"\n Entered expression is : "<<expn;
+
+bool stack::isempty() {
+    if (top == -1) {
+        return true;  // Stack is empty
+    }
+    return false;  // Stack is not empty
 }
-void paranthesis::checkexpn()
-{
-int i,flag;
-char ch;
-flag=0;
-for(i=0;expn[i]!='\0';i++)
-{
-if(expn[i]=='{' || expn[i]=='[' || expn[i]=='(')
-{
-obj.push(expn[i]);
+
+bool stack::isfull() {
+    if (top >= 24) {
+        return true;  // Stack is full
+    }
+    return false;  // Stack is not full
 }
-if(expn[i]=='}' || expn[i]==']' || expn[i]==')')
-{
-if(!obj.isempty())
-{
-ch=obj.pop();
-if(expn[i]=='}'&& ch!='{')
-{
-flag=1;
-break;
+
+void stack::push(char val) {
+    if (isfull()) {
+        cout << "\nStack is full!";
+    } else {
+        top++;
+        s[top] = val;  // Add element to stack
+    }
 }
-if(expn[i]==']'&&ch!='[')
-{
-flag=1;
-break;
+
+char stack::pop() {
+    if (isempty()) {
+        cout << "\nStack is empty!";
+        return '*';  // Returning a placeholder for empty stack
+    } else {
+        char val = s[top];
+        top--;  // Remove element from stack
+        return val;
+    }
 }
-if(expn[i]==')'&&ch!='(')
-{
-flag=1;
-break;
+
+void stack::display() {
+    if (isempty()) {
+        cout << "\nStack is empty!";
+    } else {
+        cout << "\nStack elements: ";
+        for (int i = 0; i <= top; i++) {
+            cout << s[i] << " ";
+        }
+    }
 }
+
+class paranthesis {
+    char expn[25];
+    stack obj;
+
+public:
+    void read();
+    void checkexpn();
+};
+
+void paranthesis::read() {
+    cout << "\nEnter the expression: ";
+    cin >> expn;
+    cout << "\nEntered expression is: " << expn;
 }
+
+void paranthesis::checkexpn() {
+    int i, flag = 0;
+    char ch;
+    for (i = 0; expn[i] != '\0'; i++) {
+        // Push opening brackets onto the stack
+        if (expn[i] == '{' || expn[i] == '[' || expn[i] == '(') {
+            obj.push(expn[i]);
+        }
+
+        // Check closing brackets and match them
+        if (expn[i] == '}' || expn[i] == ']' || expn[i] == ')') {
+            if (!obj.isempty()) {
+                ch = obj.pop();
+                if ((expn[i] == '}' && ch != '{') ||
+                    (expn[i] == ']' && ch != '[') ||
+                    (expn[i] == ')' && ch != '(')) {
+                    flag = 1;
+                    break;  // If mismatched parentheses, exit
+                }
+            } else {
+                flag = 1;
+                break;  // Closing without matching opening parenthesis
+            }
+        }
+    }
+
+    if (flag == 0 && obj.isempty()) {
+        cout << "\nExpression is well-formed with parentheses.";
+    } else {
+        cout << "\nExpression is not well-formed with parentheses.";
+    }
 }
+
+int main() {
+    paranthesis obj1;
+    obj1.read();
+    obj1.checkexpn();
+    return 0;
 }
-if(flag==0 && obj.isempty())
-cout<<"\n expression is in well paranthesis";
-else
-}
-cout<<"\n expression is not well paranthesis";
-int main()
-{
-paranthesis obj1;
-obj1.read();
-obj1.checkexpn();
-return 0;
-}******
